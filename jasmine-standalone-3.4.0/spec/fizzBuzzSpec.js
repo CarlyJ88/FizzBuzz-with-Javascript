4590e0561053cbf125596139a8f011fa0edc7643
Carly <carly.jenkinson65@googlemail.com>
2019-05-02 14:33:21 +0100

    passed first test

---
+++ jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
@@ -0,0 +1,16 @@
+/*
+Print the numbers from 1 to 100
+If a number is divisible by 3 print "Fizz" instead
+If a number is divisible by 5 print "Buzz" instead
+If a number is divisible by 3 and 5 print "FizzBuzz" instead
+*/
+
+describe("FizzBuzz", function(){
+  var fizzbuzz = new FizzBuzz();
+
+  describe("divisible by 3", function() {
+    it("returns true when number is divisible by 3", function() {
+      expect(fizzbuzz.divisibleByThree()).toEqual(true);
+    });
+  });
+});
---
+++ jasmine-standalone-3.4.0/src/fizzBuzz.js
@@ -0,0 +1,6 @@
+function FizzBuzz() {
+};
+
+FizzBuzz.prototype.divisibleByThree = function() {
+  return true;
+};
